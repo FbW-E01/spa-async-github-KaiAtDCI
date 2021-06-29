d5c208bbac6e8a66fd5e4f6b1dff4757c572da71
KaiAtDCI <kaiatdci@web.de>
2021-06-29 16:11:52 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+async function fetchUserRepos(username) {
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+        return await response.json()
+    } catch (error) {
+        console.warn(error);
+        throw error;
+    }
+}
+
+async function injectUserRepos(userRepos) {
+    const element = document.querySelector('#userRepositoryList');
+    userRepos.forEach(repository => {
+        const template = `
+          <div class="repository">
+            <div class="wrapper">
+              <span class="full_name">${repository.full_name}</span>
+              <span class="description">${repository.description}</span>
+            </div>
+            <span class="created_at">${repository.created_at}</span>
+          </div>
+        `
+        element.insertAdjacentHTML('beforeend', template);
+    })
+}
+
+const submitButton = document.querySelector('#submitButton');
+submitButton.addEventListener('click', () => {
+    const username = document.querySelector('#usernameInput').value;
+    fetchUserRepos(username)
+        .then(injectUserRepos);
+})
+
+
+
+
